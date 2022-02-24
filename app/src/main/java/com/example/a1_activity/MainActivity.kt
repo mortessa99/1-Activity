952156265cca0dd0f04d1e6a6925779ab2eb6f6c
mortessa99 <smortezajahan@gmail.com>
2022-02-25 03:21:07 +0330

    a

--- app/src/main/java/com/example/a1_activity/MainActivity.kt
+++ app/src/main/java/com/example/a1_activity/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.a1_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a1_activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener {
+            //binding.txtShow.setText( "Hello" + binding.edtName.text)
+            binding.txtShow.text = "Hello" + binding.edtName.text
+        }
+
     }
 }
